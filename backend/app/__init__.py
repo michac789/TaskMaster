0480ed7abcd38ab760c9cadc32d3ceed607d5f39
michac789 <michaelac978@gmail.com>
2024-03-14 14:26:43 +0800

    chore: backend define Task schema, setup flask migrations

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -9,6 +10,7 @@ SQLITE3_DB_PATH = os.path.join(CURR_DIR, 'db.sqlite3')
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{SQLITE3_DB_PATH}'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 with app.app_context():
     db.create_all()
