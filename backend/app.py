cb6eeb5630066effcd8948553aa062d5348e9956
michac789 <michaelac978@gmail.com>
2024-03-14 13:59:56 +0800

    chore: backend setup sqlalchemy, add gitingore

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+CURR_DIR = os.path.abspath(os.path.dirname(__file__))
+SQLITE3_DB_PATH = os.path.join(CURR_DIR, 'db.sqlite3')
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{SQLITE3_DB_PATH}'
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/')
 def main():
