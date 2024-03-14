8ff8cabc584d083c9c9c1da603cfb350390dc823
michac789 <michaelac978@gmail.com>
2024-03-14 23:28:56 +0800

    test: backend sample data mixin, user api test cases

--- backend/tests/__init__.py
+++ backend/tests/__init__.py
@@ -3,5 +3,11 @@ from app import create_app
 
 test_app = create_app('test')
 
-from .test_task import TaskTestCase
 from .test_ping import PingTestCase
+from .test_task import TaskTestCase
+from .test_user import (
+    UserCreateTestCase,
+    UserLoginTestCase,
+    UserGetTestCase,
+    UserChangePasswordTestCase,
+)
