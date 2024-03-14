28ceb29ea27f4146312459167b813cd89da6ffb4
michac789 <michaelac978@gmail.com>
2024-03-14 18:43:02 +0800

    test: backend refactoring, setup test app with different db

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,15 +4,24 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 
+db = SQLAlchemy()
+
 CURR_DIR = os.path.abspath(os.path.dirname(__file__))
 SQLITE3_DB_PATH = os.path.join(CURR_DIR, 'db.sqlite3')
+SQLITE3_TEST_DB_PATH = os.path.join(CURR_DIR, 'test.sqlite3')
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{SQLITE3_DB_PATH}'
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-with app.app_context():
-    db.create_all()
-
-from app import models, routes
+def create_app(type='dev'):
+    app = Flask(__name__)
+    if type == 'dev':
+        app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{SQLITE3_DB_PATH}'
+    elif type == 'test':
+        app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{SQLITE3_TEST_DB_PATH}'
+        app.config['TESTING'] = True
+    else:
+        raise ValueError('Invalid `type` given')
+    db.init_app(app)
+    Migrate(app, db)
+    with app.app_context():
+        from . import routes
+        db.create_all()
+    return app
