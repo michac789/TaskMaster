12c1e94663cbdb805e08a788b4ffc2888ea3f399
michac789 <michaelac978@gmail.com>
2024-03-14 21:24:32 +0800

    chore: backend add comments on endpoints functionality

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -4,11 +4,20 @@ from app.models import db, Task, User
 from app.utils import auth_required
 
 
+'''
+    Root endpoint, simply to ping the server.
+'''
 @app.route('/')
 def main():
     return {'message': 'Task Master API'}, 200
 
 
+'''
+    Create a new user, taking in a username and password.
+    Store the hashed password in the database.
+    Return 400 along with the validation errors if the input is invalid.
+    Return 201 and the created user if the user is created successfully.
+'''
 @app.route('/users', methods=['POST'])
 def create_user():
     data = request.json
@@ -28,6 +37,11 @@ def create_user():
     return new_user.serialize(), 201
 
 
+'''
+    Authenticate a user by username and password.
+    Return 401 if the username or password is invalid.
+    Return 200 and the jwt if the user is authenticated successfully.
+'''
 @app.route('/login', methods=['POST'])
 def login():
     data = request.json
@@ -38,12 +52,22 @@ def login():
     return {'token': token}, 200
 
 
+'''
+    Simply get the current user id and username.
+    Return 401 if the user is not authenticated.
+    Return 200 and the current user's id and username.
+'''
 @app.route('/me', methods=['GET'])
 @auth_required
 def get_user(user):
     return user.serialize(), 200
 
 
+'''
+    Get all tasks for the current user.
+    Return 401 if the user is not authenticated.
+    Return 200 and list of tasks created by the current user.
+'''
 @app.route('/tasks', methods=['GET'])
 @auth_required
 def get_all_tasks(user):
@@ -51,6 +75,12 @@ def get_all_tasks(user):
     return [task.serialize() for task in tasks], 200
 
 
+'''
+    Create a new task, use the current user id as the user_id.
+    Return 401 if the user is not authenticated.
+    Return 400 along with the validation errors if the input is invalid.
+    Return 201 and the created task if the task is created successfully.
+'''
 @app.route('/tasks', methods=['POST'])
 @auth_required
 def create_task(user):
@@ -70,6 +100,13 @@ def create_task(user):
     return new_task.serialize(), 201
 
 
+'''
+    Get a task by id.
+    Return 401 if the user is not authenticated.
+    Return 404 if the task id is invalid.
+    Return 403 if the task does not belong to the current user.
+    Return 200 and the task if the task is found.
+'''
 @app.route('/tasks/<int:task_id>', methods=['GET'])
 @auth_required
 def get_task(user, task_id):
@@ -81,12 +118,22 @@ def get_task(user, task_id):
     return task.serialize(), 200
 
 
+'''
+    Update a task by id.
+    Only title, description, due_date and status can be updated.
+    Return 401 if the user is not authenticated.
+    Return 404 if the task id is invalid.
+    Return 403 if the task does not belong to the current user.
+    Return 200 and the updated task if the task is updated successfully.
+'''
 @app.route('/tasks/<int:task_id>', methods=['PUT'])
 @auth_required
 def update_task(user, task_id):
     task = Task.query.filter(user_id=user.id, id=task_id).first()
     if not task:
         return {'error': f'Task with id {task_id} not found!'}, 404
+    if task.user_id != user.id:
+        return {'error': 'Unauthorized'}, 403
     data = request.json
     task.title = data.get('title', task.title)
     task.description = data.get('description', task.description)
@@ -96,6 +143,13 @@ def update_task(user, task_id):
     return task.serialize(), 200
 
 
+'''
+    Delete a task by id.
+    Return 401 if the user is not authenticated.
+    Return 404 if the task id is invalid.
+    Return 403 if the task does not belong to the current user.
+    Return 204 if the task is deleted successfully.
+'''
 @app.route('/tasks/<int:task_id>', methods=['DELETE'])
 @auth_required
 def delete_task(user, task_id):
